5ab0d594297061fdf7b8865194dfdcbc0ec4e25b
Kuchar <84335950+Oskirrix@users.noreply.github.com>
2026-07-17 10:41:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -76,9 +76,7 @@ function checkAuth(req, res, next) {
 
 // Skrypt userscript woła to co ~60s dla aktualnie zalogowanej postaci
 app.post('/api/heartbeat', checkAuth, async (req, res) => {
-
     console.log(req.body);
-
     const { nick, accountId, world, premium, activeAddons } = req.body;
 
   if (!nick || typeof nick !== 'string' || nick.length > 100) {
